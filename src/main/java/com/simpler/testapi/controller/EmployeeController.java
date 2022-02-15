229544d29aaf395794dc6fbffde4784dea21a2d3
brettjohnson22 <br.onson@outlook.com>
2022-02-15 11:39:05 -0600

    Added security dependencies and auth models

--- src/main/java/com/simpler/testapi/controller/EmployeeController.java
+++ src/main/java/com/simpler/testapi/controller/EmployeeController.java
@@ -9,40 +9,50 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/protected")
+@Validated
 public class EmployeeController {
 
     @Autowired
     private EmployeeService service;
 
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
     @PostMapping("/addEmployee")
     public Employee addEmployee(@RequestBody Employee employee){
         return service.saveEmployee(employee);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
     @GetMapping("/employees")
     public List<Employee> findallEmployees(){
         return service.getEmployees();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
     @GetMapping("/employeeById/{id}")
     public Employee findEmployeeById(@PathVariable Integer id){
         return service.getEmployeeById(id);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteEmployee(@PathVariable Integer id){
         service.deleteEmployee(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
     
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
     @PutMapping("/update")
     public Employee updateEmployee(@RequestBody Employee employee){
         return service.updateEmployee(employee);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
     @GetMapping("/employeeByName/{name}")
     public Employee findEmployeeByName(@PathVariable String name){
         return service.getEmployeeByName(name);
---
+++ src/main/java/com/simpler/testapi/data/models/RoleName.java
@@ -0,0 +1,5 @@
+package com.simpler.testapi.data.models;
+
+public enum RoleName {
+	USER,ADMIN,MANAGER
+}
